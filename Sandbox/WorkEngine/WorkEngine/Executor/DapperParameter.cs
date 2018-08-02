cb9565676f0e3a73d8a471f86ad8b12675d55157
usausa <machi.pon@gmail.com>
2018-08-02 16:52:28 +0900

    Update work example

--- Sandbox/WorkEngine/WorkEngine/Executor/DapperParameter.cs
+++ Sandbox/WorkEngine/WorkEngine/Executor/DapperParameter.cs
@@ -1,6 +1,16 @@
 ﻿namespace WorkEngine.Executor
 {
+    using Dapper;
+
     public sealed class DapperParameter : IParameter
     {
+        private readonly DynamicParameters parameters = new DynamicParameters();
+
+        public object Value => parameters;
+
+        public void Add(string name, object value)
+        {
+            parameters.Add(name, value);
+        }
     }
 }
