dcbd638e3d8a9191c1d0c491bb1ef27c4ba45c57
usausa <machi.pon@gmail.com>
2018-08-05 17:46:16 +0900

    Update task wotk for task parameter

--- Sandbox/WorkTask/WorkTask/WorkBuildTask.cs
+++ Sandbox/WorkTask/WorkTask/WorkBuildTask.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
 namespace WorkTask
 {
     public class WorkBuildTask : Task
     {
+        [Required]
+        public ITaskItem[] SourceFiles { get; set; }
+
+        [Required]
+        public ITaskItem OutputFile { get; set; }
+
         public override bool Execute()
         {
             Log.LogMessage("Work build test.");
+            Log.LogMessage("[SourceFiles]");
+            foreach (var sourceFile in SourceFiles)
+            {
+                Log.LogMessage($"  {sourceFile.ItemSpec}");
+            }
+
             return true;
         }
     }
