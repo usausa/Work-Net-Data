f2c8b9f826b757e1c7b71c2e4910e0f1cfc022dc
usausa <machi.pon@gmail.com>
2018-08-05 18:37:12 +0900

    Update task work for parameter check

--- Sandbox/WorkTask/WorkTask/WorkBuildTask.cs
+++ Sandbox/WorkTask/WorkTask/WorkBuildTask.cs
@@ -1,26 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
-
-namespace WorkTask
+﻿namespace WorkTask
 {
+    using System.IO;
+    using System.Linq;
+
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+
     public class WorkBuildTask : Task
     {
         [Required]
-        public ITaskItem[] SourceFiles { get; set; }
+        public string BaseDirectory { get; set; }
 
         [Required]
-        public ITaskItem OutputFile { get; set; }
+        public string OutputFile { get; set; }
+
+        [Required]
+        public ITaskItem[] SourceFiles { get; set; }
 
         public override bool Execute()
         {
             Log.LogMessage("Work build test.");
-            Log.LogMessage("[SourceFiles]");
-            foreach (var sourceFile in SourceFiles)
+            //Log.LogMessage("[BaseDirectory]");
+            //Log.LogMessage(BaseDirectory);
+            //Log.LogMessage("[OutputFile]");
+            //Log.LogMessage(OutputFile);
+            //Log.LogMessage("[SourceFiles]");
+            //foreach (var sourceFile in SourceFiles)
+            //{
+            //    Log.LogMessage($"  {sourceFile.ItemSpec}");
+            //}
+
+            var baseDir = new DirectoryInfo(BaseDirectory);
+            var outputFile = new DirectoryInfo(OutputFile);
+            var sourceFiles = SourceFiles
+                .Select(x => new FileInfo(x.ItemSpec))
+                .Where(x => x.FullName.StartsWith(baseDir.FullName + "\\"));
+
+            Log.LogMessage("[baseDir]");
+            Log.LogMessage(baseDir.FullName);
+            Log.LogMessage("[outputFile]");
+            Log.LogMessage(outputFile.FullName);
+            Log.LogMessage("[sourceFiles]");
+            foreach (var sourceFile in sourceFiles)
             {
-                Log.LogMessage($"  {sourceFile.ItemSpec}");
+                Log.LogMessage(sourceFile.FullName);
             }
 
             return true;
