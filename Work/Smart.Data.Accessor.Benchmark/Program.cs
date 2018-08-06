cce26ef3131ba4d2bee2481f3137ef9f66bc6472
usausa <machi.pon@gmail.com>
2018-08-06 21:30:58 +0900

    Update work project baseline

---
+++ Work/Smart.Data.Accessor.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Smart.Data.Accessor.Benchmark
+{
+    using System;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
