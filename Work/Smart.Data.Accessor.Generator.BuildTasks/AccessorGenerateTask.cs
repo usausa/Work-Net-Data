6ff2ce6a90cc730d89e83e00f654b7f770a15e4b
usausa <machi.pon@gmail.com>
2018-08-06 22:52:27 +0900

    Update work generator project baseline

---
+++ Work/Smart.Data.Accessor.Generator.BuildTasks/AccessorGenerateTask.cs
@@ -0,0 +1,41 @@
+﻿namespace Smart
+{
+    using System.IO;
+    using System.Linq;
+
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+
+    public sealed class AccessorGenerateTask : Task
+    {
+        [Required]
+        public string BaseDirectory { get; set; }
+
+        [Required]
+        public string OutputFile { get; set; }
+
+        [Required]
+        public ITaskItem[] SourceFiles { get; set; }
+
+        public override bool Execute()
+        {
+            var baseDir = new DirectoryInfo(BaseDirectory);
+            var outputFile = new DirectoryInfo(OutputFile);
+            var sourceFiles = SourceFiles
+                .Select(x => new FileInfo(x.ItemSpec))
+                .Where(x => x.FullName.StartsWith(baseDir.FullName + "\\"));
+
+            Log.LogMessage("[baseDir]");
+            Log.LogMessage(baseDir.FullName);
+            Log.LogMessage("[outputFile]");
+            Log.LogMessage(outputFile.FullName);
+            Log.LogMessage("[sourceFiles]");
+            foreach (var sourceFile in sourceFiles)
+            {
+                Log.LogMessage(sourceFile.FullName);
+            }
+
+            return true;
+        }
+    }
+}
