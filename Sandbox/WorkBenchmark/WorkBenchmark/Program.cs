ece4117226f01eadca2987b60a5c9e18bb146931
usausa <machi.pon@gmail.com>
2018-08-06 22:57:56 +0900

    Add work benchmark project

---
+++ Sandbox/WorkBenchmark/WorkBenchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
