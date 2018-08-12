de5a6e654a55aafaaeb321ac0db1dd30d2772f67
usausa <machi.pon@gmail.com>
2018-08-12 11:24:35 +0900

    Update resolver project

---
+++ Work/Smart.Data.Accessor.Resolver/Data/Accessor/Resolver/NamedResolverConnectionManager.cs
@@ -0,0 +1,23 @@
+﻿namespace Smart.Data.Accessor.Resolver
+{
+    using System;
+    using System.Data;
+
+    using Smart.Data.Accessor.Connection;
+    using Smart.Resolver;
+
+    public sealed class NamedResolverConnectionManager : IConnectionManager
+    {
+        private readonly IKernel kernel;
+
+        public NamedResolverConnectionManager(IKernel kernel)
+        {
+            this.kernel = kernel;
+        }
+
+        public Func<IDbConnection> GetFactory(string name)
+        {
+            return kernel.Get<IConnectionFactory>(name).CreateConnection;
+        }
+    }
+}
