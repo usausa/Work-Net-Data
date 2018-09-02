cd5d89b4a25a288660ed69837ababa43e50bc00e
usausa <machi.pon@gmail.com>
2018-09-02 17:19:03 +0900

    Update work script project

--- Sandbox/WorkScript/WorkScript/Program.cs
+++ Sandbox/WorkScript/WorkScript/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
-
-namespace WorkScript
+﻿namespace WorkScript
 {
-    class Program
+    using System;
+    using System.Data;
+
+    using Dapper;
+
+    using Microsoft.CodeAnalysis.CSharp.Scripting;
+    using Microsoft.CodeAnalysis.Scripting;
+    using Microsoft.Data.Sqlite;
+
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var con = new SqliteConnection("Data Source=:memory:"))
+            {
+                con.Open();
+
+                con.Execute("CREATE TABLE TEST(ID INTEGER, DATA TEXT, PRIMARY KEY (ID))");
+
+                var options = ScriptOptions.Default
+                    .WithReferences(
+                        typeof(IDao).Assembly,
+                        typeof(IDbConnection).Assembly,
+                        typeof(SqlMapper).Assembly);
+                var script = CSharpScript.Create(
+                    "using WorkScript;" +
+                    "using System.Data;" +
+                    "using Dapper;" +
+                    "public sealed class Dao : IDao { public int Count(IDbConnection con) => con.Execute(\"SELECT COUNT(*) FROM TEST\"); }" +
+                    "Types.Add(typeof(Dao).Name, typeof(Dao));",
+                    options,
+                    typeof(ScriptGlobals));
+                script.Compile();
+
+                var globals = new ScriptGlobals();
+                script.RunAsync(globals).Wait();
+
+                var dao = (IDao)Activator.CreateInstance(globals.Types["Dao"]);
+
+                var count = dao.Count(con);
+                Console.WriteLine(count);
+            }
         }
     }
 }
