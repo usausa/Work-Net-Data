a291f48102861f81e57957bcacb4c1871621b565
usausa <machi.pon@gmail.com>
2019-06-17 13:06:38 +0900

    Update generator helper

--- Sandbox/Works2/DataLibrary/Generator/GeneratorHelper.cs
+++ Sandbox/Works2/DataLibrary/Generator/GeneratorHelper.cs
@@ -1,22 +1,26 @@
 ﻿namespace DataLibrary.Generator
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public static class GeneratorHelper
     {
-        //public static bool IsList(Type type)
-        //{
-        //    return typeof()
-        //}
+        public static bool IsListType(Type type)
+        {
+            return type.GetInterfaces().Prepend(type)
+                .Any(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IList<>));
+        }
 
-        //public static bool IsEnumerable(Type type)
-        //{
+        public static bool IsEnumerableType(Type type)
+        {
+            return type.GetInterfaces().Prepend(type)
+                .Any(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IEnumerable<>));
+        }
 
-        //}
-
-        //public static Type GetGenericElementType(Type type)
-        //{
-
-        //}
+        public static Type GetGenericElementType(Type type)
+        {
+            return type.GetGenericArguments()[0];
+        }
     }
 }
