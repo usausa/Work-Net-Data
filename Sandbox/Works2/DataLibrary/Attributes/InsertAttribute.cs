2f51b9b27ada727902bc3b41acd1b91488fbc1ad
usausa <machi.pon@gmail.com>
2019-06-22 20:23:24 +0900

    Add attribute

---
+++ Sandbox/Works2/DataLibrary/Attributes/InsertAttribute.cs
@@ -0,0 +1,22 @@
+﻿namespace DataLibrary.Attributes
+{
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Reflection;
+
+    using DataLibrary.Loader;
+    using DataLibrary.Tokenizer;
+
+    public sealed class InsertAttribute : MethodAttribute
+    {
+        public InsertAttribute()
+            : base(CommandType.Text, MethodType.Execute)
+        {
+        }
+
+        public override ICollection<Token> CreateTokens(ISqlLoader loader, MethodInfo mi)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
