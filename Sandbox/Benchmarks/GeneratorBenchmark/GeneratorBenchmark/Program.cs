cedfda7f7904388901302250f0c175a5c2a32ed4
usausa <machi.pon@gmail.com>
2019-07-11 18:08:55 +0900

    Add generator benchmark

---
+++ Sandbox/Benchmarks/GeneratorBenchmark/GeneratorBenchmark/Program.cs
@@ -0,0 +1,166 @@
+﻿namespace GeneratorBenchmark
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Diagnosers;
+    using BenchmarkDotNet.Exporters;
+    using BenchmarkDotNet.Jobs;
+    using BenchmarkDotNet.Running;
+
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+
+    using Smart.Data;
+    using Smart.Mock.Data;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+
+    public class BenchmarkConfig : ManualConfig
+    {
+        public BenchmarkConfig()
+        {
+            Add(MarkdownExporter.Default, MarkdownExporter.GitHub);
+            Add(MemoryDiagnoser.Default);
+            Add(Job.LongRun);
+        }
+    }
+
+    [Config(typeof(BenchmarkConfig))]
+    public class Benchmark
+    {
+        private IDao staticDao;
+
+        private IDao dynamicDao;
+
+        private MockDbConnection mockExecute;
+
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            mockExecute = new MockDbConnection();
+            mockExecute.SetupCommand(cmd => cmd.SetupResult(1));
+
+            var provider = new DelegateDbProvider(() => mockExecute);
+            staticDao = new StaticDao(provider);
+            dynamicDao = DaoGenerator.Create(provider);
+        }
+
+        [IterationCleanup]
+        public void IterationCleanup()
+        {
+            mockExecute.Dispose();
+        }
+
+        [Benchmark]
+        public int StaticDao() => staticDao.Execute();
+
+        [Benchmark]
+        public int DynamicDao() => dynamicDao.Execute();
+
+    }
+
+    public interface IDao
+    {
+        int Execute();
+    }
+
+    public static class DaoGenerator
+    {
+        public static IDao Create(IDbProvider provider)
+        {
+            var syntax = CSharpSyntaxTree.ParseText(Source);
+
+            var references = new HashSet<Assembly>();
+            AddReference(references, typeof(IDao).Assembly);
+            AddReference(references, typeof(IDbProvider).Assembly);
+
+            var metadataReferences = references
+                .Select(x => MetadataReference.CreateFromFile(x.Location))
+                .ToArray();
+
+            var assemblyName = Path.GetRandomFileName();
+
+            var options = new CSharpCompilationOptions(
+                OutputKind.DynamicallyLinkedLibrary,
+                optimizationLevel: OptimizationLevel.Release);
+
+            var compilation = CSharpCompilation.Create(
+                assemblyName,
+                new[] { syntax },
+                metadataReferences,
+                options);
+
+            using (var ms = new MemoryStream())
+            {
+                var result = compilation.Emit(ms);
+                if (!result.Success)
+                {
+                    return null;
+                }
+
+                ms.Seek(0, SeekOrigin.Begin);
+                var bytes = ms.ToArray();
+                var assembly = Assembly.Load(bytes);
+
+                var implementType = assembly.GetType("GeneratorBenchmark.DynamicDao");
+                return (IDao)Activator.CreateInstance(implementType, provider);
+            }
+        }
+
+        private static void AddReference(HashSet<Assembly> assemblies, Assembly assembly)
+        {
+            if (assemblies.Contains(assembly))
+            {
+                return;
+            }
+
+            assemblies.Add(assembly);
+
+            foreach (var assemblyName in assembly.GetReferencedAssemblies())
+            {
+                AddReference(assemblies, Assembly.Load(assemblyName));
+            }
+        }
+
+        private static readonly string Source = @"namespace GeneratorBenchmark
+{
+    using Smart.Data;
+
+    public sealed class DynamicDao : IDao
+    {
+        private readonly IDbProvider provider;
+
+        public DynamicDao(IDbProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public int Execute()
+        {
+            using (var con = provider.CreateConnection())
+            using (var cmd = con.CreateCommand())
+            {
+                var param1 = cmd.CreateParameter();
+                cmd.Parameters.Add(param1);
+                var param2 = cmd.CreateParameter();
+                cmd.Parameters.Add(param2);
+
+                return cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}";
+    }
+}
---
+++ Sandbox/Benchmarks/GeneratorBenchmark/GeneratorBenchmark/StaticDao.cs
@@ -0,0 +1,28 @@
+﻿namespace GeneratorBenchmark
+{
+    using Smart.Data;
+
+    public sealed class StaticDao : IDao
+    {
+        private readonly IDbProvider provider;
+
+        public StaticDao(IDbProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public int Execute()
+        {
+            using (var con = provider.CreateConnection())
+            using (var cmd = con.CreateCommand())
+            {
+                var param1 = cmd.CreateParameter();
+                cmd.Parameters.Add(param1);
+                var param2 = cmd.CreateParameter();
+                cmd.Parameters.Add(param2);
+
+                return cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
