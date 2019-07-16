fd6c3fc4dcae3d59394e47e06ed2ed71d3955623
usausa <machi.pon@gmail.com>
2019-07-16 15:39:55 +0900

    Update engine

---
+++ Sandbox/Works3/WorkGenerator/Dao.cs
@@ -0,0 +1,94 @@
+namespace WorkGenerator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.Common;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Smart.ComponentModel;
+    using Smart.Data.Accessor.Attributes;
+
+    public class MyAttribute : ResultParserAttribute
+    {
+        public override Func<object, object> CreateConverter(IComponentContainer components, Type type)
+        {
+            return x => x;
+        }
+    }
+
+    //[NamedProvider("")]
+    [Dao]
+    public interface IFullSpecDao
+    {
+        ////[NamedProvider("")]
+        //[Execute] int Execute();
+        //[Execute] Task<int> ExecuteAsync(CancellationToken cancel);
+        //[Execute] int Execute(DbConnection con);
+        //[Execute] Task<int> ExecuteAsync(DbConnection con, CancellationToken cancel);
+        //[ExecuteScalar] object ExecuteScalarObject();
+        //[ExecuteScalar] Task<object> ExecuteScalarObjectAsync();
+        ////[My]
+        //[ExecuteScalar] long ExecuteScalar();
+        //[ExecuteScalar] Task<long> ExecuteScalarAsync(CancellationToken cancel);
+        //[ExecuteScalar] long ExecuteScalar(DbConnection con);
+        //[ExecuteScalar] Task<long> ExecuteScalarAsync(DbConnection con, CancellationToken cancel);
+        [ExecuteScalarReader] IDataReader ExecuteReader();
+        [ExecuteScalarReader] Task<IDataReader> ExecuteReaderAsync(CancellationToken cancel);
+        [ExecuteScalarReader] IDataReader ExecuteReader(DbConnection con);
+        [ExecuteScalarReader] Task<IDataReader> ExecuteReaderAsync(DbConnection con, CancellationToken cancel);
+        [Query] IEnumerable<DataEntity> QueryNonBuffer();
+        [Query] Task<IEnumerable<DataEntity>> QueryNonBufferAsync(CancellationToken cancel);
+        [Query] IEnumerable<DataEntity> QueryNonBuffer(DbConnection con);
+        [Query] Task<IEnumerable<DataEntity>> QueryNonBufferAsync(DbConnection con, CancellationToken cancel);
+        //[Query] IList<DataEntity> QueryBuffer();
+        //[Query] Task<IList<DataEntity>> QueryBufferAsync(CancellationToken cancel);
+        //[Query] IList<DataEntity> QueryBuffer(DbConnection con);
+        //[Query] Task<IList<DataEntity>> QueryBufferAsync(DbConnection con, CancellationToken cancel);
+        //[QueryFirstOrDefault] DataEntity QueryFirstOrDefault();
+        //[QueryFirstOrDefault] Task<DataEntity> QueryFirstOrDefaultAsync(CancellationToken cancel);
+        //[QueryFirstOrDefault] DataEntity QueryFirstOrDefault(DbConnection con);
+        //[QueryFirstOrDefault] Task<DataEntity> QueryFirstOrDefaultAsync(DbConnection con, CancellationToken cancel);
+        //[Execute] int ExecuteEnumerable([AnsiString(3)] string[] ids);
+        //[Procedure("TEST1")] int ExecuteEx1(ProcParameter parameter, [TimeoutParameter] int timeout);
+        //[Procedure("TEST2")] void ExecuteEx2(int param1, ref int param2, out int param3);
+        //// TODO Provider, Timeout, Tx only ?
+    }
+
+    [Dao]
+    public interface IMiscDao
+    {
+        [Execute]
+        int Execute(int id, string name);
+
+        [ExecuteScalar]
+        long Count();
+
+        [ExecuteScalar]
+        long Count1(string name, string code);
+
+        [ExecuteScalar]
+        long Count2(string code);
+
+        [Execute]
+        int ExecuteIn1(string[] ids);
+
+        [Execute]
+        int ExecuteIn2(IList<string> ids);
+
+        [Query]
+        IList<DataEntity> QueryDataList(string name, string code, string order);
+
+        [Query]
+        IList<DataEntity> QueryDataList2(QueryParameter parameter);
+
+        [Insert("Data")]
+        void Insert(DataEntity entity);
+
+        [Procedure("PROC1")]
+        void Call1(ProcParameter parameter);
+
+        [Procedure("PROC2")]
+        int Call2(int param1, ref int param2, out int param3);
+    }
+}
---
+++ Sandbox/Works3/WorkGenerator/Models.cs
@@ -0,0 +1,35 @@
+namespace WorkGenerator
+{
+    using Smart.Data.Accessor.Attributes;
+
+    public class DataEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class ProcParameter
+    {
+        [Input]
+        public string InParam { get; set; }
+
+        [InputOutput]
+        public int? InOutParam { get; set; }
+
+        [Output]
+        public int OutParam { get; set; }
+
+        [ReturnValue]
+        public int Result { get; set; }
+    }
+
+    public class QueryParameter
+    {
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public string Order { get; set; }
+    }
+}
