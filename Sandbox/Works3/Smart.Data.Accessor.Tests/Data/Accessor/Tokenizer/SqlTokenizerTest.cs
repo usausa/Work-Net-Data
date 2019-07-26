d90826a33dcd9b9243152e060670373b94b9ffc0
usausa <machi.pon@gmail.com>
2019-07-26 17:20:18 +0900

    Update test

--- Sandbox/Works3/Smart.Data.Accessor.Tests/Data/Accessor/Tokenizer/SqlTokenizerTest.cs
+++ Sandbox/Works3/Smart.Data.Accessor.Tests/Data/Accessor/Tokenizer/SqlTokenizerTest.cs
@@ -66,7 +66,28 @@ namespace Smart.Data.Accessor.Tokenizer
         // IN
         //--------------------------------------------------------------------------------
 
-        // TODO
+        [Fact]
+        public void TestIn()
+        {
+            var tokenizer = new SqlTokenizer("IN /*@ ids */ ('1', '2')");
+            var tokens = tokenizer.Tokenize();
+
+            Assert.Equal(7, tokens.Count);
+            Assert.Equal(TokenType.Block, tokens[0].TokenType);
+            Assert.Equal("IN", tokens[0].Value);
+            Assert.Equal(TokenType.Comment, tokens[1].TokenType);
+            Assert.Equal("@ ids", tokens[1].Value);
+            Assert.Equal(TokenType.OpenParenthesis, tokens[2].TokenType);
+            Assert.Equal("(", tokens[2].Value);
+            Assert.Equal(TokenType.Block, tokens[3].TokenType);
+            Assert.Equal("'1'", tokens[3].Value);
+            Assert.Equal(TokenType.Comma, tokens[4].TokenType);
+            Assert.Equal(",", tokens[4].Value);
+            Assert.Equal(TokenType.Block, tokens[5].TokenType);
+            Assert.Equal("'2'", tokens[5].Value);
+            Assert.Equal(TokenType.CloseParenthesis, tokens[6].TokenType);
+            Assert.Equal(")", tokens[6].Value);
+        }
 
         //--------------------------------------------------------------------------------
         // Insert
@@ -90,24 +111,224 @@ namespace Smart.Data.Accessor.Tokenizer
         // Quote
         //--------------------------------------------------------------------------------
 
-        // TODO
+        [Fact]
+        public void TestQuote()
+        {
+            var tokenizer = new SqlTokenizer("Name = 'abc'");
+            var tokens = tokenizer.Tokenize();
+
+            Assert.Equal(3, tokens.Count);
+            Assert.Equal(TokenType.Block, tokens[0].TokenType);
+            Assert.Equal("Name", tokens[0].Value);
+            Assert.Equal(TokenType.Block, tokens[1].TokenType);
+            Assert.Equal("=", tokens[1].Value);
+            Assert.Equal(TokenType.Block, tokens[2].TokenType);
+            Assert.Equal("'abc'", tokens[2].Value);
+        }
+
+        [Fact]
+        public void TestQuoteEscaped()
+        {
+            var tokenizer = new SqlTokenizer("Name = 'abc'''");
+            var tokens = tokenizer.Tokenize();
+
+            Assert.Equal(3, tokens.Count);
+            Assert.Equal(TokenType.Block, tokens[0].TokenType);
+            Assert.Equal("Name", tokens[0].Value);
+            Assert.Equal(TokenType.Block, tokens[1].TokenType);
+            Assert.Equal("=", tokens[1].Value);
+            Assert.Equal(TokenType.Block, tokens[2].TokenType);
+            Assert.Equal("'abc'''", tokens[2].Value);
+        }
+
+        [Fact]
+        public void TestQuoteNotClosed()
+        {
+            var tokenizer = new SqlTokenizer("Name = 'abc");
+            Assert.Throws<SqlTokenizerException>(() => tokenizer.Tokenize());
+        }
+
+        [Fact]
+        public void TestQuoteEscapedNotClosed()
+        {
+            var tokenizer = new SqlTokenizer("Name = 'abc''xyz''");
+            Assert.Throws<SqlTokenizerException>(() => tokenizer.Tokenize());
+        }
 
         //--------------------------------------------------------------------------------
         // EOL
         //--------------------------------------------------------------------------------
 
-        // TODO
+        [Fact]
+        public void TestEol()
+        {
+            var tokenizer = new SqlTokenizer("SELECT\r\n  *\r\nFROM\r\n  User\r\n");
+            var tokens = tokenizer.Tokenize();
+
+            Assert.Equal(4, tokens.Count);
+            Assert.Equal(TokenType.Block, tokens[0].TokenType);
+            Assert.Equal("SELECT", tokens[0].Value);
+            Assert.Equal(TokenType.Block, tokens[1].TokenType);
+            Assert.Equal("*", tokens[1].Value);
+            Assert.Equal(TokenType.Block, tokens[2].TokenType);
+            Assert.Equal("FROM", tokens[2].Value);
+            Assert.Equal(TokenType.Block, tokens[3].TokenType);
+            Assert.Equal("User", tokens[3].Value);
+        }
+
+        [Fact]
+        public void TestEol2()
+        {
+            var tokenizer = new SqlTokenizer("SELECT\r  *\rFROM\r  User\r");
+            var tokens = tokenizer.Tokenize();
+
+            Assert.Equal(4, tokens.Count);
+            Assert.Equal(TokenType.Block, tokens[0].TokenType);
+            Assert.Equal("SELECT", tokens[0].Value);
+            Assert.Equal(TokenType.Block, tokens[1].TokenType);
+            Assert.Equal("*", tokens[1].Value);
+            Assert.Equal(TokenType.Block, tokens[2].TokenType);
+            Assert.Equal("FROM", tokens[2].Value);
+            Assert.Equal(TokenType.Block, tokens[3].TokenType);
+            Assert.Equal("User", tokens[3].Value);
+        }
+
+        [Fact]
+        public void TestEol3()
+        {
+            var tokenizer = new SqlTokenizer("SELECT\n  *\nFROM\n  User\n");
+            var tokens = tokenizer.Tokenize();
+
+            Assert.Equal(4, tokens.Count);
+            Assert.Equal(TokenType.Block, tokens[0].TokenType);
+            Assert.Equal("SELECT", tokens[0].Value);
+            Assert.Equal(TokenType.Block, tokens[1].TokenType);
+            Assert.Equal("*", tokens[1].Value);
+            Assert.Equal(TokenType.Block, tokens[2].TokenType);
+            Assert.Equal("FROM", tokens[2].Value);
+            Assert.Equal(TokenType.Block, tokens[3].TokenType);
+            Assert.Equal("User", tokens[3].Value);
+        }
 
         //--------------------------------------------------------------------------------
         // Comment
         //--------------------------------------------------------------------------------
 
-        // TODO
+        [Fact]
+        public void TestComment()
+        {
+            var tokenizer = new SqlTokenizer("WHERE /* comment */ Id = 1");
+            var tokens = tokenizer.Tokenize();
+
+            Assert.Equal(5, tokens.Count);
+            Assert.Equal(TokenType.Block, tokens[0].TokenType);
+            Assert.Equal("WHERE", tokens[0].Value);
+            Assert.Equal(TokenType.Comment, tokens[1].TokenType);
+            Assert.Equal("comment", tokens[1].Value);
+            Assert.Equal(TokenType.Block, tokens[2].TokenType);
+            Assert.Equal("Id", tokens[2].Value);
+            Assert.Equal(TokenType.Block, tokens[3].TokenType);
+            Assert.Equal("=", tokens[3].Value);
+            Assert.Equal(TokenType.Block, tokens[4].TokenType);
+            Assert.Equal("1", tokens[4].Value);
+        }
+
+        [Fact]
+        public void TestEmptyComment()
+        {
+            var tokenizer = new SqlTokenizer("WHERE /**/ Id = 1");
+            var tokens = tokenizer.Tokenize();
+
+            Assert.Equal(5, tokens.Count);
+            Assert.Equal(TokenType.Block, tokens[0].TokenType);
+            Assert.Equal("WHERE", tokens[0].Value);
+            Assert.Equal(TokenType.Comment, tokens[1].TokenType);
+            Assert.Equal(string.Empty, tokens[1].Value);
+            Assert.Equal(TokenType.Block, tokens[2].TokenType);
+            Assert.Equal("Id", tokens[2].Value);
+            Assert.Equal(TokenType.Block, tokens[3].TokenType);
+            Assert.Equal("=", tokens[3].Value);
+            Assert.Equal(TokenType.Block, tokens[4].TokenType);
+            Assert.Equal("1", tokens[4].Value);
+        }
+
+        [Fact]
+        public void TestCommentNotClosed()
+        {
+            var tokenizer = new SqlTokenizer("WHERE /* comment");
+            Assert.Throws<SqlTokenizerException>(() => tokenizer.Tokenize());
+        }
 
         //--------------------------------------------------------------------------------
         // Line comment
         //--------------------------------------------------------------------------------
 
-        // TODO
+        [Fact]
+        public void TestLineComment()
+        {
+            var tokenizer = new SqlTokenizer("WHERE--comment\r\nId = 1");
+            var tokens = tokenizer.Tokenize();
+
+            Assert.Equal(4, tokens.Count);
+            Assert.Equal(TokenType.Block, tokens[0].TokenType);
+            Assert.Equal("WHERE", tokens[0].Value);
+            Assert.Equal(TokenType.Block, tokens[1].TokenType);
+            Assert.Equal("Id", tokens[1].Value);
+            Assert.Equal(TokenType.Block, tokens[2].TokenType);
+            Assert.Equal("=", tokens[2].Value);
+            Assert.Equal(TokenType.Block, tokens[3].TokenType);
+            Assert.Equal("1", tokens[3].Value);
+        }
+
+        [Fact]
+        public void TestLastLineComment2()
+        {
+            var tokenizer = new SqlTokenizer("WHERE--comment\rId = 1");
+            var tokens = tokenizer.Tokenize();
+
+            Assert.Equal(4, tokens.Count);
+            Assert.Equal(TokenType.Block, tokens[0].TokenType);
+            Assert.Equal("WHERE", tokens[0].Value);
+            Assert.Equal(TokenType.Block, tokens[1].TokenType);
+            Assert.Equal("Id", tokens[1].Value);
+            Assert.Equal(TokenType.Block, tokens[2].TokenType);
+            Assert.Equal("=", tokens[2].Value);
+            Assert.Equal(TokenType.Block, tokens[3].TokenType);
+            Assert.Equal("1", tokens[3].Value);
+        }
+
+        [Fact]
+        public void TestLastLineComment3()
+        {
+            var tokenizer = new SqlTokenizer("WHERE--comment\nId = 1");
+            var tokens = tokenizer.Tokenize();
+
+            Assert.Equal(4, tokens.Count);
+            Assert.Equal(TokenType.Block, tokens[0].TokenType);
+            Assert.Equal("WHERE", tokens[0].Value);
+            Assert.Equal(TokenType.Block, tokens[1].TokenType);
+            Assert.Equal("Id", tokens[1].Value);
+            Assert.Equal(TokenType.Block, tokens[2].TokenType);
+            Assert.Equal("=", tokens[2].Value);
+            Assert.Equal(TokenType.Block, tokens[3].TokenType);
+            Assert.Equal("1", tokens[3].Value);
+        }
+
+        [Fact]
+        public void TestLineCommentLast()
+        {
+            var tokenizer = new SqlTokenizer("WHERE Id = 1--comment");
+            var tokens = tokenizer.Tokenize();
+
+            Assert.Equal(4, tokens.Count);
+            Assert.Equal(TokenType.Block, tokens[0].TokenType);
+            Assert.Equal("WHERE", tokens[0].Value);
+            Assert.Equal(TokenType.Block, tokens[1].TokenType);
+            Assert.Equal("Id", tokens[1].Value);
+            Assert.Equal(TokenType.Block, tokens[2].TokenType);
+            Assert.Equal("=", tokens[2].Value);
+            Assert.Equal(TokenType.Block, tokens[3].TokenType);
+            Assert.Equal("1", tokens[3].Value);
+        }
     }
 }
