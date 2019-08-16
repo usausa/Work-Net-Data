d1fa363639ebed0ac018c6fc454765aebe26f68b
usausa <machi.pon@gmail.com>
2019-08-16 22:10:23 +0900

    Delete work project

--- Sandbox/Works/WorkGenerator/WorkGenerator/Metadata/GenerateMetadata.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WorkGenerator.Metadata
-{
-    public sealed class GenerateMetadata
-    {
-        public string[] Using { get; set; }
-
-        public ClassMetadata[] Classes { get; set; }
-    }
-}
