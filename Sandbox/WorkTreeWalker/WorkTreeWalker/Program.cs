3641f2dc4e5deb307b68b22c87231b2e07d5db71
usausa <machi.pon@gmail.com>
2019-08-27 17:51:15 +0900

    Update tree walker

--- Sandbox/WorkTreeWalker/WorkTreeWalker/Program.cs
+++ Sandbox/WorkTreeWalker/WorkTreeWalker/Program.cs
@@ -10,7 +10,7 @@ namespace WorkTreeWalker
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var tree = CSharpSyntaxTree.ParseText(Source);
             var compilation = CSharpCompilation.Create("Work", syntaxTrees: new[] { tree })
@@ -20,43 +20,68 @@ namespace WorkTreeWalker
             var model = compilation.GetSemanticModel(tree);
 
             Debug.IndentSize = 2;
-            foreach (var syntax in tree.GetRoot().DescendantNodes().OfType<InvocationExpressionSyntax>())
+            foreach (var method in tree.GetRoot().DescendantNodes().OfType<MethodDeclarationSyntax>())
             {
-                var first = syntax.ChildNodes().First();
-                if ((first is IdentifierNameSyntax identifier) && (identifier.Identifier.Text == "Setup"))
+                foreach (var invocation in method.DescendantNodes().OfType<InvocationExpressionSyntax>())
                 {
-                    Debug.WriteLine("");
-                    Debug.WriteLine($"----{syntax.ToFullString().Trim()}");
-                    Tree(syntax);
+                    var first = invocation.ChildNodes().First();
+                    if ((first is IdentifierNameSyntax identifier) && (identifier.Identifier.Text == "Setup"))
+                    {
+                        Debug.WriteLine("");
+                        Debug.WriteLine($"----{invocation.ToFullString().Trim()}");
+                        Tree(invocation.ArgumentList.Arguments[0].Expression, model);
+
+                        var type = model.GetTypeInfo(invocation.ArgumentList.Arguments[0].Expression);
+                        Debug.WriteLine($"Type = {type.Type}");
+
+                        // TODO get attribute : parameter, property, property has element, method
+                    }
                 }
             }
         }
 
-        private static void Tree(SyntaxNode node)
+        private static void Tree(SyntaxNode syntax, SemanticModel model)
         {
-            Debug.WriteLine($@"{node.GetType().Name} {node.ToFullString().Trim()}");
-
             Debug.IndentLevel++;
-            foreach (var child in node.ChildNodes())
+            Debug.WriteLine($"{syntax.GetType()} : {syntax.ToFullString().Trim()}");
+            foreach (var child in syntax.ChildNodes())
             {
-                Tree(child);
+                Tree(child, model);
             }
             Debug.IndentLevel--;
         }
 
         private const string Source =
             @"
+        public class TestAttribute : System.Attribute
+        {
+            public string Value { get; }
+
+            public TestAttribute(string value)
+            {
+                Value = value;
+            }
+        }
+
         public class Target
         {
             public void Test(
                 Index index,
+                [Test(""p"")]
                 Parameter p,
+                [Test(""id"")]
                 int id,
+                [Test(""values"")]
                 int[] values,
+                [Test(""child"")]
                 ChildParameter child,
+                [Test(""children"")]
                 ChildParameter[] children,
+                [Test(""map"")]
                 System.Collections.Generic.Dictionary<int, string> map,
+                [Test(""childMap"")]
                 System.Collections.Generic.Dictionary<int, ChildParameter> childMap,
+                [Test(""nested"")]
                 System.Collections.Generic.Dictionary<int, int[]> nested)
             {
                 Setup(id);
@@ -94,6 +119,7 @@ namespace WorkTreeWalker
                 Setup(p.ChildMap[index.Get()].Id);
                 Setup(p.ChildMap[0].Id);
                 Setup(p.Nested[0][0]);
+                Setup(p.Call());
 
                 var temp = id;
                 Setup(temp);
@@ -116,24 +142,35 @@ namespace WorkTreeWalker
 
         public class ChildParameter
         {
+            [Test(""Id"")]
             public int Id { get; set; }
         }
 
         public class Parameter
         {
+            [Test(""Id"")]
             public int Id { get; set; }
 
+            [Test(""Values"")]
             public int[] Values { get; set; }
 
+            [Test(""Child"")]
             public ChildParameter Child { get; set; }
 
+            [Test(""Children"")]
             public ChildParameter[] Children { get; set; }
 
+            [Test(""Map"")]
             public System.Collections.Generic.Dictionary<int, string> Map { get; set; }
 
+            [Test(""ChildMap"")]
             public System.Collections.Generic.Dictionary<int, ChildParameter> ChildMap { get; set; }
 
+            [Test(""Nested"")]
             public System.Collections.Generic.Dictionary<int, int[]> Nested { get; set; }
+
+            [Test(""Call"")]
+            public int Call() => 0;
         }
     }
 ";
