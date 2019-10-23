e749a8b924e09130dd69ad46f2fe4175863cabc3
usausa <machi.pon@gmail.com>
2019-10-23 22:49:23 +0900

    Add result mapper cache project

---
+++ Sandbox/ResultMapperCacheWork/ResultMapperCacheWork/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+
+namespace ResultMapperCacheWork
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+
+    public sealed class Engine
+    {
+        public Func<IDataRecord, T> CreateFactory<T>()
+        {
+            return x => Activator.CreateInstance<T>();
+        }
+
+        public ResultMapperCache CreateCache() => new ResultMapperCache(this);
+    }
+
+
+    public sealed class ResultMapperCache
+    {
+        private readonly Engine engine;
+
+        public ResultMapperCache(Engine engine)
+        {
+            this.engine = engine;
+        }
+
+        public Func<IDataRecord, T> ResolveMapper<T>(IDataRecord record)
+        {
+            // TODO
+            return engine.CreateFactory<T>();
+        }
+    }
+}
