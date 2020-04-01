a3a970be9fb115ef338b1e5745cb4e6fd333e98c
usausa <machi.pon@gmail.com>
2020-04-01 21:39:00 +0900

    Update

--- Sandbox/ReaderBenchmark/SelectorTest/DefaultMappingSelector.cs
+++ Sandbox/ReaderBenchmark/SelectorTest/DefaultMappingSelector.cs
@@ -1,6 +1,21 @@
 ﻿namespace SelectorTest
 {
-    public class DefaultMappingSelector
+    using System;
+    using System.Linq;
+
+    public class DefaultMappingSelector : IMappingSelector
     {
+        public TypeMapInfo Select(Type type, ColumnInfo[] columns)
+        {
+            var columnMap = columns.ToDictionary(x => x.Name);
+
+            //var ctos = type.GetConstructors()
+            //    .OrderByDescending(x => x.GetParameters().Length)
+            //    .Where()
+
+            // TODO FindConstructor
+
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Sandbox/ReaderBenchmark/SelectorTest/Libs/Configs/EntitySuffix.cs
@@ -0,0 +1,9 @@
+namespace Smart.Data.Accessor.Configs
+{
+    using System.Collections.Generic;
+
+    public static class EntitySuffix
+    {
+        public static IReadOnlyList<string> Default { get; } = new[] { "Entity", "Model" };
+    }
+}
