1709f22e90ad1f570f62965afcaa113f880e7061
usausa <machi.pon@gmail.com>
2021-03-09 19:34:30 +0900

    Add work project

---
+++ Sandbox/WorkNullableAnalysys/NullableEnable/EnableTarget.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NullableEnable
+{
+    public class EnableTarget
+    {
+    }
+}
---
+++ Sandbox/WorkNullableAnalysys/WorkNullableAnalysis/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WorkNullableAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
