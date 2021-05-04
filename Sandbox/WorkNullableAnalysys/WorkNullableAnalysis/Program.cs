3bb869dc0e596dca5ef5f41c5a3a66c12caa5935
usausa <machi.pon@gmail.com>
2021-05-04 19:49:12 +0900

    Update work project

--- Sandbox/WorkNullableAnalysys/WorkNullableAnalysis/Program.cs
+++ Sandbox/WorkNullableAnalysys/WorkNullableAnalysis/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WorkNullableAnalysis
 {
@@ -6,6 +7,42 @@ namespace WorkNullableAnalysis
     {
         static void Main(string[] args)
         {
+            // TODO enableありのメタデータ確認
+            // TODO e -> d, d ->e の警告確認、戻り値？
+            // TODO ビルドオプション？、MSBuildなので別口？
         }
     }
+
+    //--------------------------------------------------------------------------------
+    // Interface
+    //--------------------------------------------------------------------------------
+#nullable enable
+    public interface INullableAccessor
+    {
+        T? QueryFirstOrDefault<T>();
+
+        List<T> QueryList<T>();
+
+        string? QuerySingle();
+
+        string QuerySingleNonNullable();
+
+        int Execute(string? param);
+
+        int ExecuteNonNullable(string param);
+    }
+#nullable restore
+
+#nullable disable
+    public interface INonNullableAccessor
+    {
+        T QueryFirstOrDefault<T>();
+
+        List<T> QueryList<T>();
+
+        string QuerySingle();
+
+        int Execute(string param);
+    }
+#nullable restore
 }
